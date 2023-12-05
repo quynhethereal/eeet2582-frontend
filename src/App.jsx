c5e8dfaeb4ec1b5239b86ba1b2a4647672378593
Quynh Le <quynh.helendinh@gmail.com>
2023-12-05 11:49:52 +0700

    feat(setup-google-auth): Setup login auth

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,40 @@ import HomePage from "./pages/HomePage";
 import AuthenPage from "./pages/AuthenPage";
 
 import "./App.css";
+import {useAuth} from "react-oidc-context";
 
 function App() {
-  return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />}></Route>
-          <Route path="/authentication" element={<AuthenPage />}></Route>
-        </Routes>
-      </BrowserRouter>
-    </>
-  );
+    const auth = useAuth();
+
+    switch (auth.activeNavigator) {
+        case "signinSilent":
+            return <div>Signing you in...</div>;
+        case "signoutRedirect":
+            return <div>Signing you out...</div>;
+    }
+
+    if (auth.isLoading) {
+        return <div>Loading...</div>;
+    }
+
+    if (auth.error) {
+        return <div>Oops... {auth.error.message}</div>;
+    }
+
+    if (auth.isAuthenticated) {
+        return (
+            <>
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={<HomePage />}></Route>
+                        <Route path="/authentication" element={<AuthenPage />}></Route>
+                    </Routes>
+                </BrowserRouter>
+            </>
+        );
+    }
+
+    return <button onClick={() => void auth.signinRedirect()}>Log in</button>;
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { AuthProvider } from "react-oidc-context";
+import {AuthProvider} from "react-oidc-context";
 import App from "./App.jsx";
 import "./index.css";
+
 const githubClientId = import.meta.env.VITE_GITHUB_CLIENT_ID;
 const serverOrigin = import.meta.env.VITE_SERVER_ORIGIN;
 const oauthProvider = import.meta.env.VITE_OAUTH_PROVIDER;
 const appOrigin = window.location.origin;
+const client_secret = import.meta.env.VITE_CLIENT_SECRET;
 
 const oidcConfig = {
-  authority: oauthProvider,
-  client_id: githubClientId,
-
-  redirect_uri: appOrigin,
-  response_type: "code",
-  scope: "read:user",
+    authority: oauthProvider,
+    client_id: githubClientId,
+    redirect_uri: appOrigin,
+    client_secret: client_secret,
 };
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <AuthProvider {...oidcConfig}>
-      <App />
-    </AuthProvider>
-  </React.StrictMode>
+    <React.StrictMode>
+        <AuthProvider {...oidcConfig}>
+            <App/>
+        </AuthProvider>
+    </React.StrictMode>
 );
